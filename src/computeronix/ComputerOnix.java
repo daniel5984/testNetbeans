0ee444d4306b71c25568549f2ec3f028650f6145
DanielGonçalves <>
2020-03-18 16:31:30 +0000

    isto é uma alteração de teste

---
+++ src/computeronix/ComputerOnix.java
@@ -0,0 +1,95 @@
+package computeronix;
+
+import comum.Le;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ComputerOnix {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+        
+    public class venda {
+            private int numVenda;
+            private Date data;
+            private float valor;
+            
+            public venda(int nv) {
+            numVenda = nv;
+            data = new Date();
+        }
+            public void setDate(Date d) {
+            data = d;
+        }
+            public Date getDate(){
+                return data;
+            }
+            public int getNumVenda() {
+            return numVenda;
+        }
+            public float getValor(){
+                return valor;
+            }
+            public void setValor(float val){
+                valor = val;
+            }
+    }
+    
+    public class vendaArtigo extends venda{
+        private venda[] artigo;
+
+        public vendaArtigo(int nv) {
+            super(nv);
+        }
+        
+        
+    }
+    
+    public class ColVendas{
+        venda v ;
+        int count=0;
+        ArrayList<venda> list;   //Criar uma arrayList 
+        
+        public ColVendas(){
+            
+           
+            list=new ArrayList<venda>();
+            
+        }
+        
+        public void adicionarVenda(venda v){
+            Date d = new Date();
+            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+            
+            if(list.contains(v)){
+                System.out.println("Não Pode adicionar");
+            }else{
+                
+                v = new venda(count++);
+                System.out.println("Adicionar uma venda");
+                
+                System.out.println("Qual o valor da venda?");
+                int valor= Le.umInt();
+                v.setValor(valor);
+                System.out.println(dateFormat.format(d));
+                
+                
+                System.out.println("Vou fazer uma modificação");
+                
+                
+            }
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+}
